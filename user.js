0c01f2067df59924ad86f5dbc80570c8c4e3e1eb
theluposki <theluposki@gmail.com>
2023-09-22 01:38:21 -0300

    adjusts

---
+++ user.js
@@ -0,0 +1,21 @@
+const alice = {
+  name: "Alice",
+  keys: {
+    privateKey:
+      "99396e5a9109d836554a1b2a4094c08171e937767664415853de839d64a7c6a4",
+    publicKey:
+      "0497e0507ebd1c572093aa78ee05f5659d37ae26d78d0f24edd14c2f4336b68ac12036eeb6835b48d947c8226d50a5abe1febca38cc2829b7321784afb75a52be6",
+  },
+};
+
+const bob = {
+  name: "Bob",
+  keys: {
+    privateKey:
+      "a147c294178d7411f2ba45f1f89778fee0cba4b24f71ea4b4c549a07636dfd09",
+    publicKey:
+      "0460b0bf485bd4abc0b9ab45b8c8551d9ef791954a5136b0b490aa6f7006f2976d57b66dea5bd4cc60dca41e4255949723c027d104c1735d37f6884cc57742b510",
+  },
+};
+
+export { alice, bob }
